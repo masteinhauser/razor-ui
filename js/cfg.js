4fb93a5802c055799e06888305c371bd68960cc1
Myles Steinhauser <myles.steinhauser@gmail.com>
2014-03-03 15:03:13 +0100

    Depend on ProxyPass to /api

--- js/cfg.js
+++ js/cfg.js
@@ -1,13 +1,14 @@
 var config = {};
 
 config.exdata = true;
-config.dump		= false;
+config.dump		= true;
+//config.dump		= false;
 
 if (config.exdata) {
   config.server = "localhost";
   config.port = "5000";
 } else {
   config.server = "10.128.2.2";
-  config.port = "8080";
+  config.port = "80";
 }	
 
