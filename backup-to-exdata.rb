53b11bb4f41dcdf00957fc362b426ad326f851d4
Myles Steinhauser <myles.steinhauser@gmail.com>
2014-03-06 01:54:43 +0100

    Start of link following for API download

--- backup-to-exdata.rb
+++ backup-to-exdata.rb
@@ -4,22 +4,40 @@ require "json"
 require "net/http"
 require "uri"
  
-uri = URI.parse("http://10.128.2.2:8080/api")
- 
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Get.new(uri.request_uri)
- 
-response = http.request(request)
- 
-if response.code == "200"
-  result = JSON.parse(response.body)
+collections = {}
+
+#uri = URI.parse("http://10.128.2.2:8080/api")
+uri = URI.parse("http://127.0.0.1:5000/exdata/api.json")
+
+def FindNextLink(collection, doc)
+	if doc["id"] && doc["name"]
+		collection[doc["id"]] = {name:doc["name"]}
+	end
+end
+
+def APIget(collection, uri, list)
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+   
+  response = http.request(request)
   
-  result.each do |doc|
-    puts doc["id"] #reference properties like this
-    puts doc # this is the result in object form    
-    puts ""
-    puts ""
+  if response.code == "200"
+    result = JSON.parse(response.body)
+  
+    result[list].each do |item|
+      FindNextLink(collection, item)
+    end
+  else
+    puts "ERROR!!!"
   end
-else
-  puts "ERROR!!!"
+end
+
+APIget(collections, uri, "collections")
+
+puts "Found items"
+collections.each do |id, data|
+	puts id
+	puts data[:name]
+	puts data
+	puts ""
 end
