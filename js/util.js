9153a5bc918567a8d0fb7e7b0807a2fb0edd1d93
Daniel Silverman <me@agperson.com>
2015-09-24 12:25:09 -0400

    CSS cleanup, section navigation, and Bootstrap for list views

--- js/util.js
+++ js/util.js
@@ -1,24 +1,27 @@
 var RazorAppUtil = angular.module('RazorAppUtil', []);
 
 RazorAppUtil.factory('Util',
-	function($rootScope){
-		var dumpJSON = config.dump || false;
+  function($rootScope){
+    var dumpJSON = config.dump || false;
 
-		return {
-			getRazorURL: function(url){
-		    if ( config.exdata ) {
-		      return config.prefix + "/exdata" + url + ".json";
-		    } else {
-		      return config.prefix + url;
-		    }
-		  },
-	    showJSON: function(show){
-				if( show != null ){
-					dumpJSON = show;
-				}
-			
-				return dumpJSON;
-			}
-		}
-	}
+    return {
+      getRazorURL: function(url){
+        if ( config.exdata ) {
+          return config.prefix + "/exdata" + url + ".json";
+        } else {
+          return config.prefix + url;
+        }
+      },
+      showJSON: function(show){
+        if( show != null ){
+          dumpJSON = show;
+        }
+
+        return dumpJSON;
+      },
+      isActiveNav: function(navPath) {
+        return (navPath === $location.path());
+      }
+    }
+  }
 );
